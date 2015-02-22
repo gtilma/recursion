25990dd0acf39f630c43c4a206629fd706dbb2b6
Gerald Tilma <gerald.tilma@gmail.com>
2015-02-22 13:20:25 -0600

    stringifyJSON passing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,23 +6,47 @@
 var stringifyJSON = function(obj) {
   var result = '';
 
-  var toJSONstring = function(obj) {
-    if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
-      result += obj;
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    result += obj;
+  }
+
+  if (typeof obj === 'string') {
+    result += '\"' + obj + '\"';
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 1) {
+      result += '[' + stringifyJSON(obj[0]) + ']';
     }
-    if (typeof obj === 'string') {
-      result += '\"' + obj + '\"';
-    }
-    if (Array.isArray(obj)) {
-      if (obj.length > 0) {
-        for (var i = 0; i < obj.length; i++) {
-          result += '[' + toJSONstring(obj[i]) + ']';
+    if (obj.length > 1) {
+      result += '[';
+      for (var i = 0; i < obj.length; i++) {
+        result += stringifyJSON(obj[i]);
+        if (i < obj.length - 1) {
+          result += ',';
         }
-      } else {
-        result += '[]';
       }
+      result += ']';
     }
-  };
-  toJSONstring(obj);
+    if (obj.length === 0) {
+      result += '[]';
+    }
+  }
+
+  if (typeof obj === 'object' && !Array.isArray(obj) && obj !== null) {
+    var objArray = [];
+    result += '{';
+
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof (obj[key]) === 'undefined') {
+        continue;
+      }
+      objArray.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    result += objArray.join();
+
+    result += '}';
+  }
+
   return result;
 };
